1623cf423de1c2c216bf2af94d152bb6e622df40
bunnerfly <68405838+bunnerfly@users.noreply.github.com>
2026-04-23 00:15:31 -0600

    Add StLaunchController

---
+++ Source/Entities/StLaunchController.cs
@@ -0,0 +1,35 @@
+using Celeste.Mod.Roslyn.ModLifecycleAttributes;
+
+namespace Celeste.Mod.HamburgerHelper.Entities;
+
+[CustomEntity("HamburgerHelper/StLaunchController")]
+public class StLaunchController : Entity
+{
+    public StLaunchController(EntityData data, Vector2 offset) 
+        : base(data.Position + offset)
+    {
+    }
+    
+    [OnLoad]
+    internal static void Load()
+    {
+        On.Celeste.Player.LaunchUpdate += PlayerOnLaunchUpdate;
+    }
+
+    [OnUnload]
+    internal static void Unload()
+    {
+        On.Celeste.Player.LaunchUpdate -= PlayerOnLaunchUpdate;
+    }
+    
+    private static int PlayerOnLaunchUpdate(On.Celeste.Player.orig_LaunchUpdate orig, Player self)
+    {
+        // just to add it to the tracker for sure :sobs:
+        self.Scene.Tracker.GetEntitiesTrackIfNeeded<StLaunchController>();
+        
+        int controllerCount = self.Scene.Tracker.CountEntities<StLaunchController>();
+        if (controllerCount == 0) return orig(self);
+
+        return self.onGround ? Player.StNormal : orig(self);
+    }
+}
